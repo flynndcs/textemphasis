03c42562fcc87892d99b8ece8a982749905be3a5
flynndcs <flynndcs@gmail.com>
2018-02-03 10:09:16 -0600

    Add files via upload
    fix error in previous commit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ var data = [
     'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo',
     'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'
 ]; 
-
-var multipleStyles = []
+var pickedColor;
 // displays text on page
 function createDivs() {
 	var container = $("#textContent");
@@ -26,10 +25,19 @@ function insertText() {
 }
 
 
+
+
+// listens for color change, sets to variable to be used in further click listener
+
 //clicklisteners for divs, toggle class for text style
 // calls function
+// createDivs();
+// insertText();
+// changeColor();
 createDivs();
 insertText();
+
+
 
 // for loop - click listener for text styles
 $('#bold').on("click", function (){
@@ -44,9 +52,52 @@ $('#underline').on("click", function (){
 	$('#underline').toggleClass("enabled");
 });
 
-// click listener for divs, use .hasClass()
-for(var i = 0; i < data.length; i++) {
+// take color from input, use it as option to change text
+
+
+for(let i = 0; i < data.length; i++) {
   $('#div' + i).click( function(){
-    $('#div' + i).toggleClass(multipleStyles())
-  });
+    if ($("#bold").hasClass("enabled")) {
+    	$('#div' + i).toggleClass("bold");
+    } else {
+    	console.log("nah");
+    }
+});
+}
+
+for(let i = 0; i < data.length; i++) {
+  $('#div' + i).click( function(){
+    if ($("#italic").hasClass("enabled")) {
+    	$('#div' + i).toggleClass("italic");
+    } else {
+    	console.log("nah")
+    }
+});
+}
+for(let i = 0; i < data.length; i++) {
+  $('#div' + i).click( function(){
+    if ($("#underline").hasClass("enabled")){
+    	$('#div' + i).toggleClass("underline");
+    } else {
+    	console.log("nah")
+    }
+});
+}
+
+$("#color-picker").on("input", function(event) {
+        pickedColor = event.target.value;
+        console.log(pickedColor);
+});
+
+$("#color-clear").on("click", function(){
+    pickedColor = '';
+    console.log(pickedColor);
+});
+
+
+for(let i = 0; i < data.length; i++) {
+    $('#div' + i).click( function() {
+        
+        $('#div' + i).css("color", pickedColor);
+    })
 }
